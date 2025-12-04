4005c74f4364ab86f738a0e148ae2af7250333b4
JIbraimov <jibraimov@wooppay.com>
2025-12-04 09:49:56 +0500

    Vercel config

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,9 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
 
 
 
